087f7f902ec80741993e80d7dae21bfe531fc26d
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-19 22:11:21 -0500

    feat: create CityInfo component

---
+++ src/components/CityInfo/Cityinfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CityInfo = ({city, country}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+CityInfo.propTypes = {
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired
+}
+
+export default CityInfo
