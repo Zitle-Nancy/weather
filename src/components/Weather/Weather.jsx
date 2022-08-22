53cf9784478b10a2ef84cf189947ca082d75e8d7
Nancy Zitle <nancy.zitle@creditas.com>
2022-08-21 21:35:16 -0500

    feat: validate type of weather

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@mui/material/Typography';
-import { WiCloud } from 'react-icons/wi'
+import { WiCloud, WiDayCloudy,WiDayFog, WiDaySunny,WiRain } from 'react-icons/wi'
 import { IconContext } from 'react-icons';
 const Weather = ({temperature}) => {
   return (
