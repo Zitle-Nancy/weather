0dd50bf1e3752016adfa655c670b438e8dc0b2dc
Nancy Zitle <nancy.zitle@creditas.com>
2022-09-06 07:00:56 -0500

    feat: add remainder to create test in ForecastChart component

--- src/components/Forecastchart/ForecastChart.test.jsx
+++ src/components/Forecastchart/ForecastChart.test.jsx
@@ -2,6 +2,7 @@ import {render} from '@testing-library/react';
 
 import ForecastChart from './ForecastChart'
 
+// @TODO: Create test for this component
 test('ForecastChart render', async () => {
  
 })
