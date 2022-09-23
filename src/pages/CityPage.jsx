161ceb9eb10304d7c1785dd36dae37a61c6ce95a
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-09-22 22:33:01 -0500

    feat: install react-router and delete files and folder unnecessary

---
+++ src/pages/CityPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CityPage = (props) => {
+  return <div>CityPage</div>;
+};
+
+CityPage.propTypes = {};
+
+export default CityPage;
---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WelcomePage = (props) => {
+  return <div>WelcomePage</div>;
+};
+
+WelcomePage.propTypes = {};
+
+export default WelcomePage;
