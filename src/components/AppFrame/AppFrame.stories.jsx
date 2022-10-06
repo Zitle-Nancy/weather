aa38ff751f8a6abbb3882faa13219e944c8940c1
Nancy Zitle <nancyzitle@MacBook-Pro-de-Nancy.local>
2022-10-05 21:44:14 -0500

    feat(App): create AppFrame component and we use children react props

---
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -0,0 +1,12 @@
+import AppFrame from "./AppFrame";
+import { BrowserRouter as Router } from "react-router-dom";
+export default {
+  title: "AppFrame",
+  component: <AppFrame />,
+};
+
+export const AppFrameExample = () => (
+  <Router>
+    <AppFrame>text</AppFrame>
+  </Router>
+);
